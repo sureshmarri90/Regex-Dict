5a784dfb9bd9f891c5b595e147f49db84aa8c9cd
sureshmarri90 <marrisureshyadav@gmail.com>
2018-02-26 20:13:51 -0500

    Printed value as string

--- Regexp_25.py
+++ Regexp_25.py
@@ -1,10 +1,13 @@
 # split()
 # The Regex Version
 
+#this is the last example
+
 import re
 line = 'From stephen.marquard@uct.ac.za Sat Jan 5 09:14:16 2008'
 y=re.findall('^From .*@([^ ]*)',line)
 print(y)
+print(str(y))
 
 
              
